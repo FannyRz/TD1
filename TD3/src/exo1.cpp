7618df6c18a31b7f86b8d0c2e14911705d97c892
FannyRz <fanny.rz.celia@gmail.com>
2024-03-28 14:44:39 +0100

    exo 1

--- TD3/src/exo1.cpp
+++ TD3/src/exo1.cpp
@@ -2,19 +2,22 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <stack>
+#include <iterator>
+#include <cctype>
 
-std::string saisie_utilisateur(std::string saisie){
+std::string saisie_utilisateur(){
 
-    std::string saisie;
-    std::cout << "Entrez votre expression arithmétique" << std::endl;
+    std::string saisie {};
+    std::cout << "Entrez votre expression arithmetique" << std::endl;
     std::getline(std::cin, saisie);
     return saisie;
 
 }
 
-std::vector<std::string> separation_saisie(std::string const& saisie){
+std::vector<std::string> separation_saisie(std::string const& s){
 
-    std::istringstream in(saisie); // transforme une chaîne en flux de caractères, cela simule un flux comme l'est std::cin
+    std::istringstream in(s); // transforme une chaîne en flux de caractères, cela simule un flux comme l'est std::cin
     // l’itérateur va lire chaque element de "in", comme un flux d'entrée, chaque élément est séparé par un espace
     return std::vector<std::string>(std::istream_iterator<std::string>(in), std::istream_iterator<std::string>());
 
@@ -32,14 +35,51 @@ bool est_flottant(std::string const& s){
 }
 
 
+float calcule (float rightOperand,float leftOperand,std::string const s){
+    if(s==std::string{'+'}){
+        return leftOperand+rightOperand;
+    }else if(s==std::string{'-'}){
+        return leftOperand-rightOperand;
+    }else if(s==std::string{'*'}){
+        return leftOperand*rightOperand;
+    }else if(s==std::string{'/'}){
+        return leftOperand/rightOperand;
+    }else{
+        std::cout << "operateur inconnu" << std::endl;
+        return -1.f;
+    }
+}
+
 float npi_evaluate(std::vector<std::string> const& tokens){
 
+    std::stack<float> stack;
+
+    for(std::string s : tokens){
+
+        if(est_flottant(s)){
+            stack.push(std::stof(s));
+        }else{
+            // Je récupère l'élément en haut de la pile
+            float rightOperand { stack.top() };
+            // Je l'enlève de la stack (la méthode top ne fait que lire l’élément en dessus de la pile)
+            stack.pop();
+            float leftOperand { stack.top() };
+            stack.pop();
+
+            // Il faut ensuite en fonction de l'opérateur calculer le résultat pour le remettre dans la pile
+            float result {calcule(rightOperand,leftOperand,s) };
+            stack.push(result);
+        }        
+    }
+    return stack.top();
 
 }
 
-
 int main()
 {
-
-
+    std::string saisie {saisie_utilisateur()};
+    std::vector<std::string> tab {separation_saisie(saisie)};
+    float result {npi_evaluate(tab)};
+    std::cout << "Le resultat de " << saisie << " est : " << result << std::endl;
+    
 }
