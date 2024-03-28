1fec2af895dc0e774a4eeb263b93f00416f07d60
FannyRz <fanny.rz.celia@gmail.com>
2024-03-28 11:37:07 +0100

    question 1-3

--- TD3/src/exo1.cpp
+++ TD3/src/exo1.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <sstream>
+
+std::string saisie_utilisateur(std::string saisie){
+
+    std::string saisie;
+    std::cout << "Entrez votre expression arithmétique" << std::endl;
+    std::getline(std::cin, saisie);
+    return saisie;
+
+}
+
+std::vector<std::string> separation_saisie(std::string const& saisie){
+
+    std::istringstream in(saisie); // transforme une chaîne en flux de caractères, cela simule un flux comme l'est std::cin
+    // l’itérateur va lire chaque element de "in", comme un flux d'entrée, chaque élément est séparé par un espace
+    return std::vector<std::string>(std::istream_iterator<std::string>(in), std::istream_iterator<std::string>());
+
+}
+
+bool est_flottant(std::string const& s){
+
+    for(char c : s){
+        if(!(std::isdigit(c)  || c=='.')){
+            return false;
+        }
+    }
+    return true;
+    
+}
+
+
+float npi_evaluate(std::vector<std::string> const& tokens){
+
+
+}
 
 
 int main()
 {
+
 
 }
