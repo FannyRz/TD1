23d208f0822cd01618997436b65fb677580071ba
FannyRz <fanny.rz.celia@gmail.com>
2024-04-09 19:49:37 +0200

    exo 2 Q2

--- TD4/src/exo2.cpp
+++ TD4/src/exo2.cpp
@@ -20,13 +20,35 @@ int firstWord (std::string const & str){
 }
 
 std::vector<std::string> split_string(std::string const& str){
-    std::vector<std::string> str {};
+
+    // Permet de savoir si un element est un espace
+    auto const is_space = [](char letter){ return letter == ' '; };
+
+    std::vector<std::string> tab {};
+    auto it_begin {str.begin()};
+    auto it_end {std::find_if(str.begin(),str.end(), is_space)};
+    while(it_begin != str.end()){
+        std::string mot {};
+        std::copy(it_begin, it_end, std::back_inserter(mot));
+        tab.push_back(mot);
+        if(it_end == str.end()){
+            break;
+        }
+        it_begin = it_end + 1;
+        it_end = std::find_if(it_begin,str.end(), is_space);
+    }
+    return tab;
 }
 
 
 int main(){
 
     std::string const str {"bonjour je suis fanny"};
-    std::cout << "Le premier mot de la phrase est de longueur : " << firstWord(str) << std::endl;
+    // std::cout << "Le premier mot de la phrase est de longueur : " << firstWord(str) << std::endl;
+    
+    std::vector<std::string> result {split_string(str)};
+    for(std::string mot : result){
+        std::cout << mot << std::endl;
+    }
 
 }
