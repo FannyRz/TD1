3a6522050ae9bcdb4ad3e543aae3780936cbdf0d
FannyRz <fanny.rz.celia@gmail.com>
2024-05-01 16:51:53 +0200

    TP6 bonne création

---
+++ TD6/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "node.hpp"
+
+int main()
+{
+
+    return 0;
+}
---
+++ TD6/src/node.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+
+struct Node {
+    int value;
+    Node* left { nullptr };
+    Node* right { nullptr };
+};
