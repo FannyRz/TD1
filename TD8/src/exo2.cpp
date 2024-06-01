089b6f5c044bd384c5371fe934dbeacec10a7297
FannyRz <fanny.rz.celia@gmail.com>
2024-06-01 16:56:56 +0200

    EXO 1 Q1

--- TD8/src/exo2.cpp
+++ TD8/src/exo2.cpp
@@ -1,54 +1,37 @@
 #include <iostream>
-#include <vector>
-#include <numeric>
-#include <algorithm>
-#include <cstdlib>
+#include <string>
+#include <unordered_map>
 
-int firstWord (std::string const & str){
-    int longueur {0};
-
-    // Permet de savoir si un element est un espace
-    auto const is_space = [](char letter){ return letter == ' '; };
-
-    // Recherche de l'élément " " dans str
-    auto it {std::find_if(str.begin(),str.end(), is_space)};
-    if(it == str.end()){
-        return str.size();
-    }else{
-        return std::distance(str.begin(),it);
+std::unordered_map<char, size_t> frequency(std::string str){
+    std::unordered_map<char, size_t> res {};
+    for(char c : str){
+        std::pair<char, size_t> courant {};
+        if(res.find(c) == res.end()){
+            courant.first = c;
+            int compteur {0};
+            for(int i{0}; i<str.size();i++){
+                if(str[i]==c){
+                    compteur++;
+                }
+            }
+            courant.second = compteur;
+            res.insert(courant);            
+        }
     }
+    return res;
 }
 
-std::vector<std::string> split_string(std::string const& str){
-
-    // Permet de savoir si un element est un espace
-    auto const is_space = [](char letter){ return letter == ' '; };
-
-    std::vector<std::string> tab {};
-    auto it_begin {str.begin()};
-    auto it_end {std::find_if(str.begin(),str.end(), is_space)};
-    while(it_begin != str.end()){
-        std::string mot {};
-        std::copy(it_begin, it_end, std::back_inserter(mot));
-        tab.push_back(mot);
-        if(it_end == str.end()){
-            break;
-        }
-        it_begin = it_end + 1;
-        it_end = std::find_if(it_begin,str.end(), is_space);
+void display_unordered_map(std::unordered_map<char, size_t> map){
+    for(std::unordered_map<char,size_t>::iterator it {map.begin()}; it != map.end(); it++){
+        std::cout << it->first << " : " << it->second << std::endl ;
     }
-    return tab;
 }
 
 
 int main(){
+    std::string str {"coucou les copains"};
+    display_unordered_map(frequency(str));
 
-    std::string const str {"bonjour je suis fanny"};
-    // std::cout << "Le premier mot de la phrase est de longueur : " << firstWord(str) << std::endl;
-    
-    std::vector<std::string> result {split_string(str)};
-    for(std::string mot : result){
-        std::cout << mot << std::endl;
-    }
+    return 0;
 
 }
